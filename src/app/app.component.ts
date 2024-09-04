5ceac3451640b149a92ca351f5fcf2719fd9c014
gabrielGC <gabriel225gc@gmail.com>
2024-09-04 15:11:35 -0300

    Feat: recebendo dados do backend

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { EnderecoService } from './services/endereco.service';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'endereco-viacep';
+  endereco: any;
+
+  constructor(private enderecoService: EnderecoService) { }
+
+  buscarEndereco(cep: string) {
+    this.enderecoService.buscarEndereco(cep).subscribe({
+      next: data => this.endereco = data,
+      error: e => console.error("Erro ao buscar o endereço ", e)
+    });
+  }
 }
--- src/app/services/endereco.service.ts
+++ src/app/services/endereco.service.ts
@@ -1,9 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
+interface Endereco {
+  cep: string;
+  logradouro: string;
+  complemento: string;
+  bairro: string;
+  localidade: string;
+  uf: string;
+  ibge: string;
+  gia: string;
+  ddd: string;
+  siafi: string;
+}
 @Injectable({
   providedIn: 'root'
 })
 export class EnderecoService {
 
-  constructor() { }
+  private apiURL = "http://localhost:8080/api/enderecos";
+
+  constructor(private http: HttpClient) { }
+
+  buscarEndereco(cep: string): Observable<Endereco> {
+    const url = `${this.apiURL}/${cep}`;
+    return this.http.get<Endereco>(url);
+  }
 }
